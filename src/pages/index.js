1fd4152f966467d9eb335851b9869dd0c1d3f8c7
HaysS <stanfordhays@gmail.com>
2018-11-12 12:57:33 -0600

    changed header

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+    <h1>Sup homies</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
